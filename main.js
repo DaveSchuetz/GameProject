f9439f9f92fbba7e1e709903a9ee468c7be9a01f
David Schuetz <38507773+DaveSchuetz@users.noreply.github.com>
2018-07-13 00:56:31 -0400

    Final game

--- main.js
+++ main.js
@@ -1,71 +1,83 @@
 // setting the towers and disk vars
-const towers = document.querySelectorAll('.tower')
+const tower1 = document.querySelector('#first')
+const tower2 = document.querySelector('#second')
+const tower3 = document.querySelector('#third')
 const diskSelector = 3
-// const diskOrder = []
+const diskOrder = []
+let gameTower = [ [], [], [] ]
 //Creating using DOM elements
 for (i = 1; i <= diskSelector; i++){
     let diskDiv = document.createElement('div')
     diskDiv.id = 'disk' + i
     diskDiv.className = 'disk'
-    towers[0].appendChild(diskDiv)
-    // diskOrder.push(diskDiv.id)
+    tower1.appendChild(diskDiv)
+    diskOrder.push(diskDiv.id)
 }
-const disks = document.querySelectorAll('.disk')
-let gameTower = [ [], [], [] ]
-
-for (i=0;i<disks.length;i++){
-    gameTower[0].push(disks[i])
-}
-let lastTower1 = gameTower[0][gameTower[0].length - 1]
-let lastTower2 = gameTower[1][gameTower[1].length - 1]
-let lastTower3 = gameTower[2][gameTower[2].length - 1]
 
 //Showing minimum moves on board
-let minMoves = document.getElementById('minimum')
-minMoves = 2 ** disks.length - 1
+let minMoves = 2 ** diskOrder.length - 1
 document.getElementById('minimum').textContent = minMoves
+// move counter
+let counter = 0
+
+
 
 //Click event to show highlighted item
 let active = false
-lastTower1.addEventListener('click', () => {
-    if (lastTower1.style.borderWidth != '10px'){
-        lastTower1.style.borderWidth = '10px'
-        active = lastTower1
+tower1.addEventListener('click', () => {
+    if (active === tower1.lastChild){
+        tower1.lastChild.style.borderWidth = '1px'
+        active = false
+    }else if (active === false){
+        tower1.lastChild.style.borderWidth = '10px'
+        active = tower1.lastChild
+    }else if (active.offsetWidth < tower1.lastChild.offsetWidth || tower1.childElementCount === 0){
+        active.style.borderWidth = '1px'
+        tower1.appendChild(active)
+        counter =counter + 1
+        document.getElementById('counter').textContent = counter
+        active = false
     }else{
-        lastTower1.style.borderWidth = '1px'
-        active = false
-    }
+        active.style.borderWidth = '1px'
+        active = false}
 })
-
-// lastTower2.addEventListener('click',() => {
-//     if (lastTower2.style.borderWidth != '10px'){
-//         lastTower2.style.borderWidth = '10px'
-//         active = true
-//     }else{
-//         lastTower2.style.borderWidth = '1px'
-//         active = false
-//     }
-// })
-
-// lastTower1.addEventListener('click',() => {
-//     if (lastTower3.style.borderWidth != '10px'){
-//         lastTower3.style.borderWidth = '10px'
-//         active = true
-//     }else{
-//         lastTower3.style.borderWidth = '1px'
-//         active = false
-//     }
-// })
-
-// moving to new tower
-towers[1].addEventListener('click', () => {
-    if (active != false){
-        gameTower[1].push(lastTower1)
-        lastTower1.style.borderWidth = '1px'
-        gameTower[0].pop()
-        towers[1].appendChild(document.getElementById('disk3'))
-        lastTower1 = gameTower[0][gameTower[0].length - 1]
+tower2.addEventListener('click', () => {
+    if (active === tower2.lastChild){
+        tower2.lastChild.style.borderWidth = '1px'
         active = false
-    }
-    towers[1].removeEventListener('click', () => {active = false})
+    }else if (active === false){
+        tower2.lastChild.style.borderWidth = '10px'
+        active = tower2.lastChild
+    }else if (active.offsetWidth < tower2.lastChild.offsetWidth || tower2.childElementCount === 0){
+        active.style.borderWidth = '1px'
+        tower2.appendChild(active)
+        counter =counter + 1
+        document.getElementById('counter').textContent = counter
+        active = false
+    }else{
+        active.style.borderWidth = '1px'
+        active = false}
 })
+tower3.addEventListener('click', () => {
+    if (active === tower3.lastChild){
+        tower3.lastChild.style.borderWidth = '1px'
+        active = false
+    }else if (active === false){
+        tower3.lastChild.style.borderWidth = '10px'
+        active = tower3.lastChild
+    }else if (active.offsetWidth < tower3.lastChild.offsetWidth || tower3.childElementCount === 0){
+        active.style.borderWidth = '1px'
+        tower3.appendChild(active)
+        counter =counter + 1
+        document.getElementById('counter').textContent = counter
+        active = false
+        winner()
+    }else{
+        active.style.borderWidth = '1px'
+        active = false}
+})
+function winner() {if (tower3.childElementCount === 3){
+    alert(`Congratulations!!! You completed the tower in ${counter} number of moves`)
+}}
+
+
